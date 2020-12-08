2c42f3eb1c203a20d173c05f1b3d3e6738158c84
dasprasen <pd18041997@gmail.com>
2020-12-08 21:48:35 +0530

    Added Front-End Design for Different Component.

--- src/main/webapp/reactjs/src/components/BookList.js
+++ src/main/webapp/reactjs/src/components/BookList.js
@@ -1,8 +1,34 @@
-import React from "react";
+import React, {Component} from "react";
+import { Card, Table } from "react-bootstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faList} from "@fortawesome/free-solid-svg-icons";
 
-class BookList extends React.Component{
+class BookList extends Component{
 	render(){
-		return(<div className="text-white">Book List</div>);
+		return(
+			 <Card className={"border border-dark bg-dark text-white"}>
+				 <Card.Header><FontAwesomeIcon icon={faList} /> Book List</Card.Header>
+				 <Card.Body>
+					 <Table striped bordered hover variant="dark">
+						 <thead>
+							<tr>
+								<th>Title</th>
+								<th>Author</th>
+								<th>ISBN Number</th>
+								<th>Price</th>
+								<th>Language</th>
+							    <th>Actions</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr align="center">
+								<td colSpan="6">No Books Available</td>
+							</tr>
+						</tbody>
+					 </Table>
+				 </Card.Body>
+			 </Card>
+		);
 	}
 }
 

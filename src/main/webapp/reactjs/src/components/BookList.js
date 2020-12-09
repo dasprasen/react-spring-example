279f4ddc99b1d343a07e4e0b285552924baa4b81
dasprasen <pd18041997@gmail.com>
2020-12-09 22:43:40 +0530

    Added Delete and Add Functionality using Axios.

--- src/main/webapp/reactjs/src/components/BookList.js
+++ src/main/webapp/reactjs/src/components/BookList.js
@@ -3,6 +3,7 @@ import { Card, Table, Image, Button, ButtonGroup } from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faList, faEdit, faTrash} from "@fortawesome/free-solid-svg-icons";
 import axios from 'axios';
+import Mytoast from './MyToast';
 
 class BookList extends Component{
 
@@ -25,50 +26,70 @@ class BookList extends Component{
 			});
 	}
 
+	deleteBook = (bookId) => {
+		axios.delete("http://localhost:8080/rest/books/"+bookId)
+			.then(response => {
+				if(response.data != null){
+					this.setState({"show":true});
+					setTimeout(() => this.setState({"show":false}),3000);
+					this.setState({
+						books: this.state.books.filter(book => book.id !== bookId)
+					});
+				}else{
+					this.setState({"show":false});
+				}
+			});
+	}
+
 	render(){
 		return(
-			 <Card className={"border border-dark bg-dark text-white"}>
-				 <Card.Header><FontAwesomeIcon icon={faList} /> Book List</Card.Header>
-				 <Card.Body>
-					 <Table striped bordered hover variant="dark">
-						 <thead>
-							<tr>
-								<th>Title</th>
-								<th>Author</th>
-								<th>ISBN Number</th>
-								<th>Price</th>
-								<th>Language</th>
-							    <th>Actions</th>
-							</tr>
-						</thead>
-						<tbody>
-							{
-								this.state.books.length === 0 ?
-								<tr align="center">
-									<td colSpan="6"> Books Available</td>
-								</tr> :
-								this.state.books.map((book) => (
-									<tr key={book.id}>
-										<td>
-											<Image src={book.coverPhotoURL} roundedCircle width="25" height="25"/>
-											{book.title}</td>
-										<td>{book.author}</td>
-										<td>{book.isbnNumber}</td>
-										<td>{book.price}</td>
-										<td>{book.language}</td>
-										<td>
-											<ButtonGroup>
-												<Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit} /></Button>
-												<Button size="sm" variant="outline-danger"><FontAwesomeIcon icon={faTrash} /></Button>
-											</ButtonGroup>
-										</td>
-									</tr>
-								))
-							}
-						</tbody>
-					 </Table>
-				 </Card.Body>
-			 </Card>
+			<div>
+				<div style={{"display":this.state.show ? "block" : "none"}}>
+					<Mytoast children = {{show:this.state.show, message : "Book Deleted Successfully", type:"danger"}}/>
+				</div>
+					<Card className={"border border-dark bg-dark text-white"}>
+					<Card.Header><FontAwesomeIcon icon={faList} /> Book List</Card.Header>
+					<Card.Body>
+						<Table striped bordered hover variant="dark">
+							<thead>
+								<tr>
+									<th>Title</th>
+									<th>Author</th>
+									<th>ISBN Number</th>
+									<th>Price</th>
+									<th>Language</th>
+									<th>Actions</th>
+								</tr>
+							</thead>
+							<tbody>
+								{
+									this.state.books.length === 0 ?
+									<tr align="center">
+										<td colSpan="6"> Books Available</td>
+									</tr> :
+									this.state.books.map((book) => (
+										<tr key={book.id}>
+											<td>
+												<Image src={book.coverPhotoURL} roundedCircle width="25" height="25"/>
+												{book.title}</td>
+											<td>{book.author}</td>
+											<td>{book.isbnNumber}</td>
+											<td>{book.price}</td>
+											<td>{book.language}</td>
+											<td>
+												<ButtonGroup>
+													<Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit} /></Button>
+													<Button size="sm" variant="outline-danger" onClick={this.deleteBook.bind(this, book.id)}><FontAwesomeIcon icon={faTrash} /></Button>
+												</ButtonGroup>
+											</td>
+										</tr>
+									))
+								}
+							</tbody>
+						</Table>
+					</Card.Body>
+				</Card>
+			</div>
 		);
 	}
 }
